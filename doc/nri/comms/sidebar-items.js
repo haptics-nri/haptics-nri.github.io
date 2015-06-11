ca2674402c14deee158602f7af00295be5831e15
Alex Burka <aburka@seas.upenn.edu>
2015-06-10 23:01:21 -0400

    cargo doc

--- doc/nri/comms/sidebar-items.js
+++ doc/nri/comms/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"trait":[["Controllable",""]]});
+initSidebarItems({"trait":[["Controllable","A service that can be setup and torn down based on commands from a higher power."]]});
